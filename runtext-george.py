6875cedfff80e71e988758b2f984189f6addb3a4
gyroworld <dieselg5@gmail.com>
2021-12-29 13:37:43 -0500

    Update runtext-george.py

--- runtext-george.py
+++ runtext-george.py
@@ -15,17 +15,17 @@ options.disable_hardware_pulsing = True
 matrix = RGBMatrix(options = options)
 
 #Set font
-font_file = "/home/pi/rpi-rgb-led-matrix/fonts/7x13.bdf"
-font = graphics.Font().LoadFont(font_file)
+font = graphics.Font()
+font.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/7x13.bdf")
 
 #Set color
 textColor = graphics.Color(255, 0, 255)
 
 #Drawing on screen
 offscreen_canvas = matrix.CreateFrameCanvas()
-#offscreen_canvas.Clear()
+offscreen_canvas.Clear()
 graphics.DrawText(offscreen_canvas, font, 32, 10, textColor, "Test")
-#matrix.SwapOnVSync(offscreen_canvas)
+matrix.SwapOnVSync(offscreen_canvas)
 
 print('ran')
 time.sleep(5)
