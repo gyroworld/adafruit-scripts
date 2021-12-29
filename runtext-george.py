642ecf2918d4b2564d8d2a240ca2e84277c0c5fd
gyroworld <dieselg5@gmail.com>
2021-12-29 13:43:50 -0500

    Update runtext-george.py

--- runtext-george.py
+++ runtext-george.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
-#import sys
-#sys.path.append('/home/pi/rpi-rgb-led-matrix/bindings/python/samples')
-#from samplebase import SampleBase
+
 from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 import time
 
@@ -30,36 +28,3 @@ matrix.SwapOnVSync(offscreen_canvas)
 print('ran')
 time.sleep(5)
 print('complete')
-
-
-"""
-class RunText(SampleBase):
-    def __init__(self, *args, **kwargs):
-        super(RunText, self).__init__(*args, **kwargs)
-        self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
-
-    def run(self):
-        offscreen_canvas = self.matrix.CreateFrameCanvas()
-        font = graphics.Font()
-        font.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/7x13.bdf")
-        textColor = graphics.Color(255, 0, 255)
-        pos = offscreen_canvas.width
-        my_text = self.args.text
-
-        while True:
-            offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
-            pos -= 1
-            if (pos + len < 0):
-                pos = offscreen_canvas.width
-
-            time.sleep(0.05)
-            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
-
-
-# Main function
-if __name__ == "__main__":
-    run_text = RunText()
-    if (not run_text.process()):
-        run_text.print_help()
-"""
