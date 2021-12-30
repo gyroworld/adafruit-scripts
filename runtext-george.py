9f2f49d9fa9c4455e0a4e2b91eac5e781851f7e3
gyroworld <dieselg5@gmail.com>
2021-12-30 08:59:07 -0500

    Update runtext-george.py

--- runtext-george.py
+++ runtext-george.py
@@ -12,29 +12,38 @@ options.hardware_mapping = 'adafruit-hat'
 options.disable_hardware_pulsing = True
 
 
-#Create canvas
-canvas = RGBMatrix(options = options)
-offscreen_canvas = canvas.CreateFrameCanvas()
+#Create matrix and offscreen canvas
+matrix = RGBMatrix(options = options)
+offscreen_canvas = matrix.CreateFrameCanvas()
 
 #Set font
 font = graphics.Font()
 font.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/7x13.bdf")
 
 #Set color
-textColor = graphics.Color(255, 0, 255)
+purple = graphics.Color(255, 0, 255)
+red = graphics.Color(255, 0, 0)
+
 
 
 #Draw line
 #DrawLine(core.Canvas c, int x1, int y1, int x2, int y2, Color color)
-#graphics.DrawLine(canvas, 1, 1, 10, 10, textColor)
+#graphics.DrawLine(matrix, 1, 1, 10, 10, textColor)
 #Drawing on screen
 #time.sleep(5)
 
-offscreen_canvas.Clear()
 #DrawText(core.Canvas c, Font f, int x, int y, Color color, text):
-graphics.DrawText(canvas, font, 1, 10, textColor, "Test")
-canvas.SwapOnVSync(offscreen_canvas)
-
-print('ran')
+print("Running test1.")
+graphics.DrawText(matrix, font, 1, 10, purple, "Test1")
 time.sleep(5)
-print('complete')
+print("Test1 complete.")
+
+print("Running test2.")
+i = 0
+while i < 10:
+    offscreen_canvas.Clear()
+    graphics.DrawText(offscreen_canvas, font, 1, 10, red, "Test2")
+    time.sleep(0.5)
+    offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
+    i += 1
+print("Test2 complete.")
