368534a7e95609e9a9f3c1306df151d1cd802124
gyroworld <dieselg5@gmail.com>
2021-12-31 15:23:36 -0500

    Update led_matrix_fireworks.py

--- led_matrix_fireworks.py
+++ led_matrix_fireworks.py
@@ -252,7 +252,7 @@ while True:
             matrix.SetPixel(x-3, y+3, 0, 0, 0)
 
         #offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
-        sleep(.5)
+        sleep(.1)
 
     else:
         #offscreen_canvas.Clear()
