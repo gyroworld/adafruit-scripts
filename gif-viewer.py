73d51360bb17b81a18109a64bb3e0031d42b371b
gyroworld <dieselg5@gmail.com>
2021-12-30 20:29:59 -0500

    Update gif-viewer.py

--- gif-viewer.py
+++ gif-viewer.py
@@ -18,7 +18,7 @@ matrix = RGBMatrix(options = options)
 
 while True:
     for frame in range(0,14):
-        image_file = f"'/home/pi/adafruit-scripts/frame00{frame}.png'"
+        image_file = f"'/home/pi/adafruit-scripts/frames{frame}.png'"
         image = Image.open(image_file)
         image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
         matrix.SetImage(image.convert('RGB'))
