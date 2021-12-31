815b46b220f168c433840812dd70f65f31dd6996
gyroworld <dieselg5@gmail.com>
2021-12-31 10:48:51 -0500

    Update runtext-george.py

--- runtext-george.py
+++ runtext-george.py
@@ -40,7 +40,7 @@ black = graphics.Color(0, 0, 0)
 
 def timeLeft():
     #jan012022 = datetime(2022, 1, 1, 0, 0, 0, 0)
-    jan012022 = datetime(2021, 12, 31, 10, 47, 0, 0)
+    jan012022 = datetime(2021, 12, 31, 10, 51, 0, 0)
     now = datetime.now()
     diff = jan012022 - now
     hours = int(diff.seconds/3600)
