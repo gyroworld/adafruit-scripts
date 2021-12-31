580019f3fd6bcde74352f333bac5315107595263
gyroworld <dieselg5@gmail.com>
2021-12-30 21:07:06 -0500

    Update runtext-george.py

--- runtext-george.py
+++ runtext-george.py
@@ -24,7 +24,7 @@ font = graphics.Font()
 font.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/6x10.bdf")
 
 font_small = graphics.Font()
-font_small.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/5x7.bdf")
+font_small.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/4x6.bdf")
 
 #Set colors
 purple = graphics.Color(255, 0, 255)
