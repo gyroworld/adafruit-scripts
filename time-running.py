6ca51fdad37b5b181922a3ecf1011fcb80ce3e13
gyroworld <dieselg5@gmail.com>
2022-01-22 17:47:48 -0500

    Update time-running.py

--- time-running.py
+++ time-running.py
@@ -11,7 +11,6 @@ options.rows = 32
 options.chain_length = 1
 options.parallel = 1
 options.hardware_mapping = 'adafruit-hat'
-options.led_slowdown_gpio = 0
 options.show_refresh_rate = True
 options.gpio_slowdown = 0
 options.disable_hardware_pulsing = False
